4b1451d01149f1f8a8812507b8b65b5aba749638
rusekronos <65853763+rusekronos@users.noreply.github.com>
2020-05-26 18:56:57 +1000

    Update timer2.js

--- Scripts/timer2.js
+++ Scripts/timer2.js
@@ -1,8 +1,12 @@
 let next = 0;
       let times = [];
       let today = new Date();
+      function day() {return today.getDay();}
+      let dateNamesFrom = {"sunday":0, "monday":1, "tuesday":2, "wednesday":3,
+			   "thursday":4, "friday":5, "saturday":6}
+      let dateNamesTo = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday",
+			 "Friday", "Saturday"]
       today.setHours(0,0,0,0);
-      console.log(today)
       function timeTil() {
           return (times[next].timeFrom + today.getTime()) - Date.now();
       }
@@ -23,7 +27,25 @@ let next = 0;
           console.assert(splut[1] < 60, "Valid time string 3");
           return 1000*(splut[0]*60*60 + splut[1]*60);
       }
-      function update() {
+      function gen_table(json) {
+	  table = document.getElementById("times");
+          tstr = "";
+          for(const [k, v] of Object.entries(json.timetableData[dateNamesTo[day()].toLowerCase()])) {
+              tstr += "<tr><td>";
+              tstr += k;
+              tstr += "</td><td>";
+              tstr += v.startTime;
+              tstr += "</td></tr>";
+              times.push({periodName: k, timeFrom: timeStringToMS(v.startTime)});
+          }
+          table.innerHTML = tstr;
+	  times.sort(function(a, b) {return a.timeFrom - b.timeFrom;});
+      }
+
+      function updateDay() {
+	  document.getElementById("day").innerHTML = dateNamesTo[day()];
+      }
+      function update(json) {
           timer = document.getElementById("timer");
           period = document.getElementById("period");
           let tt = timeTil();
@@ -34,30 +56,25 @@ let next = 0;
                   //this only occurs in the event that
                   //the next period is tomorrow
                   next = 0;
+		  times = [];
                   today.setDate(today.getDate()+1); //tomorrow comes today
+		  gen_table(json);
+		  updateDay();
               }
               tt = timeTil();
           }
           timer.innerHTML = timeTilHMS();
           period.innerHTML = times[next].periodName;
       }
+
       let xhr = new XMLHttpRequest();
       xhr.responseType = 'json';
-      xhr.open('GET', './Scripts/timetableProto1.json', true);
+      xhr.open('GET', 'bellTimes.json', true);
       xhr.onload = function () {
           json = xhr.response;
-          table = document.getElementById("times");
-          tstr = "";
-          for(const [k, v] of Object.entries(json.timetableData)) {
-              tstr += "<tr><td>";
-              tstr += k;
-              tstr += "</td><td>";
-              tstr += v.startTime;
-              tstr += "</td></tr>";
-              times.push({periodName: k, timeFrom: timeStringToMS(v.startTime)});
-          }
-          table.innerHTML = tstr;
-          times.sort(function(a, b) {return a.timeFrom - b.timeFrom;});
+	  gen_table(json);
+	  updateDay();
+	  update(json);
           window.setInterval(update, 1000);
       };
       xhr.send();
