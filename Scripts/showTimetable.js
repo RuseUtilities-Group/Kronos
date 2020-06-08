fccccb63be019bca3cff53755f926f209b0df0ce
rusekronos <65853763+rusekronos@users.noreply.github.com>
2020-06-08 13:43:03 +1000

    Update showTimetable.js

--- Scripts/showTimetable.js
+++ Scripts/showTimetable.js
@@ -1,4 +1,4 @@
 var timetable = document.getElementById("timetableDiv");
 if (localStorage.getItem("personalTimetable") !== null) {
-	timetable.href = "https://rusekronos.xyz/timetable"
+	timetable.href = "./timetable.html"
 }
