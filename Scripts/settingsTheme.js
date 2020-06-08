bbeac2e7a9ea448ddf40279acb7750729f0e0a83
rusekronos <65853763+rusekronos@users.noreply.github.com>
2020-06-09 09:34:44 +1000

    Update settingsTheme.js

--- Scripts/settingsTheme.js
+++ Scripts/settingsTheme.js
@@ -4,18 +4,18 @@ const currentTheme = localStorage.getItem('theme');
 if (currentTheme) {
     document.documentElement.setAttribute('data-theme', currentTheme);
   
-    if (currentTheme === 'dark') {
+    if (currentTheme === 'default') {
         toggleSwitch.checked = true;
     }
 }
 
 function switchTheme(e) {
     if (e.target.checked) {
-        document.documentElement.setAttribute('data-theme', 'dark');
-        localStorage.setItem('theme', 'dark');
+        document.documentElement.setAttribute('data-theme', 'default');
+        localStorage.setItem('theme', 'light');
     }
-    else {document.documentElement.setAttribute('data-theme', 'default');
-          localStorage.setItem('theme', 'light');
+    else {document.documentElement.setAttribute('data-theme', 'dark');
+          localStorage.setItem('theme', 'dark');
     }    
 }
 
