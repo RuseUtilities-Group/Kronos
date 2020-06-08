2239697bca5e3b154110972d6504e74da950f07e
iamsosmart19 <iamsosmart19@gmail.com>
2020-06-08 18:31:54 +1000

    Capitalised days of the week

--- Scripts/timetable.js
+++ Scripts/timetable.js
@@ -18,7 +18,7 @@ function gen_table(json) {
 			tableIn += "<tr id=\"Weeks\">";
 		}
 		tableIn += "<td id=\"timetableTd\"><table id=\"timetableDay\">";
-		tableIn += `<tr><th>${listOfDays[day].substring(0, listOfDays[day].length-1)}</th></tr>`;
+		tableIn += `<tr><th>${listOfDays[day].substring(0, listOfDays[day].length-1)[0].toUpperCase() + listOfDays[day].substring(0, listOfDays[day].length-1).slice(1)}</th></tr>`;
 		period = 1;
 		while(typeof it[listOfDays[day]][`Period ${period}`] != "undefined") {
 			tableIn += "<tr>";
