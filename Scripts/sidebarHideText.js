c08b26c73c58121e0eeb7a5a37ff45588de45412
rusekronos <65853763+rusekronos@users.noreply.github.com>
2020-06-12 20:24:46 +1000

    Update sidebarHideText.js

--- Scripts/sidebarHideText.js
+++ Scripts/sidebarHideText.js
@@ -1,5 +1,59 @@
 var screenWidth = screen.width;
 var maxIconShowWidth = 1375;
+//home icon loooool
+var a = document.getElementById("homeIcon");
 if(screenWidth > 1375){
+    a.innerHTML = "";
+}
+else {
+    a.innerHTML = "Home";
+}
+//Bell Times Icon looooool
+var a = document.getElementById("belltimerIcon");
+if(screenWidth > 1375){
+    a.innerHTML = "";
+}
+else {
+    a.innerHTML = "Bell Times";
+}
 
+//Timetable Icon looooool
+var a = document.getElementById("timetableIcon");
+if(screenWidth > 1375){
+    a.innerHTML = "";
+}
+else {
+    a.innerHTML = "Timetable";
+}
+
+var a = document.getElementById("calendarIcon");
+if(screenWidth > 1375){
+    a.innerHTML = "";
+}
+else {
+    a.innerHTML = "Calendar";
+}
+
+var a = document.getElementById("linksIcon");
+if(screenWidth > 1375){
+    a.innerHTML = "";
+}
+else {
+    a.innerHTML = "Links";
+}
+
+var a = document.getElementById("staffIcon");
+if(screenWidth > 1375){
+    a.innerHTML = "";
+}
+else {
+    a.innerHTML = "Staff List";
+}
+
+var a = document.getElementById("settingsIcon");
+if(screenWidth > 1375){
+    a.innerHTML = "";
+}
+else {
+    a.innerHTML = "Settings";
 }
