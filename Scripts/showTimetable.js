23beee6603c4aea5108932cf3164c5210b185a49
rusekronos <65853763+rusekronos@users.noreply.github.com>
2020-06-17 10:14:49 +1000

    Update showTimetable.js

--- Scripts/showTimetable.js
+++ Scripts/showTimetable.js
@@ -1,8 +1,11 @@
 var timetable = document.getElementById("timetableDiv");
-var staff = document.getElementById("staffDiv");
+var login = document.getElementById("loginDiv");
 
 if (localStorage.getItem("personalTimetable") !== null) {
 	timetable.href = "./timetable.html"
-
+	login.href = "./times.html"
 }
 
+if (localStorage.getItem("personalTimetable") === null) {
+	login.href = "./timesNoTimetableDetected.html"
+}
